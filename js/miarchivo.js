411d783b0004f3dab1c47252458957592f990472
canda13 <candeabalosa2001@gmail.com>
2023-04-11 23:22:29 -0300

    pre-entrega 1

---
+++ js/miarchivo.js
@@ -0,0 +1,34 @@
+alert("Buenas!! Para calcular el promedio de su asignatura haga click en aceptar!");
+
+let asignatura = prompt("Escriba el nombre de la asignatura:");
+
+function cuenta(){
+    let cuenta = 0;
+
+    for(i = 0; i < 3; i++){
+        calculo = parseInt(prompt("Por favor ingrese tres calificaciones:"));
+        
+        while(cuenta < 0 || cuenta > 10){
+            alert("Nota no valida. Recordá que los números permitidos son del 1 al 10. Ingrese otra vez.");
+            nota = parseInt(prompt("Ingrese la nota:"))
+        }
+        
+        cuenta = cuenta + calculo;
+    }
+
+    return total = cuenta / 3;
+}
+    
+let promedio = cuenta();
+
+if(promedio >= 8){
+    alert("Felicidades! El promedio de " + asignatura + " es: " + promedio + ". Podes irte de vacaciones al caribe!!.");
+} 
+else{
+    alert("Lo siento. Tu promedio de " + asignatura + " es: " + promedio + ". Lo siento. Tendras que quedarte en casa estudiando para el final!, fuerzas!!");
+}
+    
+
+
+
+
